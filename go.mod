743b1b00d71acb49ee8a8da72d702faa9c9cccaa
Ryan Petris <ryan@petris.net>
2024-03-15 16:45:37 -0700

    Just use vercmp as go-alpm hasn't been updated to the latest version of libalpm

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/ryanpetris/aur-builder
 go 1.21.0
 
 require (
-	github.com/Jguer/go-alpm/v2 v2.2.1
 	github.com/glebarez/go-sqlite v1.22.0
 	github.com/go-git/go-git/v5 v5.8.1
 	gopkg.in/yaml.v3 v3.0.1
@@ -29,6 +28,7 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/skeema/knownhosts v1.2.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
--- pacman/version.go
+++ pacman/version.go
@@ -1,11 +1,32 @@
 package pacman
 
 import (
-	"github.com/Jguer/go-alpm/v2"
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func IsVersionNewer(oldVersion string, newVersion string) (bool, error) {
-	result := alpm.VerCmp(oldVersion, newVersion)
+	outBuf := &bytes.Buffer{}
 
-	return result < 0, nil
+	cmd := exec.Command("vercmp", oldVersion, newVersion)
+	cmd.Stdout = outBuf
+
+	err := cmd.Run()
+
+	if err != nil {
+		fmt.Println(outBuf.String())
+		return false, err
+	}
+
+	valueStr := strings.SplitN(outBuf.String(), "\n", 2)[0]
+	value, err := strconv.Atoi(valueStr)
+
+	if err != nil {
+		return false, err
+	}
+
+	return value < 0, nil
 }
